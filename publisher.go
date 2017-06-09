f46af426e82c164e529745f2b125db7d1ff4dd92
Felix S <felix@gnampfelix.de>
2017-06-09 20:31:57 +0000

    Remove any network ability
    To simplify the package pub, all networking features are removed. They
    will be re-introduced soonly over a new type/package that is designed to
    work well together with the current pub package.

--- publisher.go
+++ publisher.go
@@ -1,66 +1,28 @@
 package pub
 
 import (
-	"bytes"
 	"io"
-	"net"
-	"strconv"
 	"sync"
 )
 
 //  A Publisher can publish messages to different subscribers.
 type Publisher interface {
-	Gather(port int) error
 	Publish(message Message)
 	Subscribe(tag string, subCreater func() Subscriber) Subscriber
-	AddRemote(remoteAddress string)
-	CancelGathering(port int)
 	Unsubscribe(tag string, subscriber Subscriber)
 }
 
 type publisher struct {
 	subscribers map[string][]Subscriber
 	sync.RWMutex
-	remotes         []string
-	cancelGathering map[int]bool
 }
 
 func New() Publisher {
 	subs := make(map[string][]Subscriber)
-	remotes := make([]string, 0)
-	cancelGathering := make(map[int]bool)
 	result := publisher{
-		subscribers:     subs,
-		remotes:         remotes,
-		cancelGathering: cancelGathering,
+		subscribers: subs,
 	}
 	return &result
-}
-
-func (p *publisher) Gather(port int) error {
-	p.cancelGathering[port] = false
-	listener, err := net.Listen("tcp", ":"+strconv.Itoa(port))
-	if err != nil {
-		return err
-	}
-	for {
-		conn, err := listener.Accept()
-		if p.cancelGathering[port] {
-			conn.Close()
-			listener.Close()
-			return nil
-		}
-		if err != nil {
-			continue
-		}
-		go p.handleMessage(conn)
-	}
-}
-
-func (p *publisher) CancelGathering(port int) {
-	p.cancelGathering[port] = true
-	conn, _ := net.Dial("tcp", ":"+strconv.Itoa(port))
-	defer conn.Close()
 }
 
 func (p *publisher) Publish(message Message) {
@@ -68,35 +30,7 @@ func (p *publisher) Publish(message Message) {
 	defer p.Unlock()
 	message.Close()
 
-	remoteTemplate := NewMessage(message.Tag())
-	localTemplate := io.TeeReader(message, remoteTemplate)
-	p.publishLocal(message.Tag(), localTemplate)
-	p.publishRemote(message.Tag(), remoteTemplate, message.Sender())
-}
-
-func (p *publisher) publishRemote(tag string, payload io.Reader, sender string) {
-	tagLength := len(tag)
-	header := make([]byte, 0)
-	for i := tagLength / 255; i > 0; i-- {
-		header = append(header, 255)
-	}
-	header = append(header, byte(tagLength%255))
-	header = append(header, []byte(tag)...)
-	var writers []io.Writer
-	for i := range p.remotes {
-		if p.remotes[i] == sender {
-			continue
-		}
-		currentConn, err := net.Dial("tcp", p.remotes[i])
-		defer currentConn.Close()
-		if err != nil {
-			continue
-		}
-		writers = append(writers, currentConn.(io.Writer))
-	}
-	writer := io.MultiWriter(writers...)
-	writer.Write(header)
-	io.Copy(writer, payload)
+	p.publishLocal(message.Tag(), message)
 }
 
 func (p *publisher) publishLocal(tag string, payload io.Reader) {
@@ -128,12 +62,6 @@ func (p *publisher) Subscribe(tag string, subCreater func() Subscriber) Subscrib
 	return result
 }
 
-func (p *publisher) AddRemote(remoteAddress string) {
-	p.Lock()
-	defer p.Unlock()
-	p.remotes = append(p.remotes, remoteAddress)
-}
-
 func (p *publisher) Unsubscribe(tag string, subscriber Subscriber) {
 	subs, ok := p.subscribers[tag]
 	subPosition := -1
@@ -148,27 +76,4 @@ func (p *publisher) Unsubscribe(tag string, subscriber Subscriber) {
 			p.subscribers[tag] = append(subs[:subPosition], subs[subPosition+1:]...)
 		}
 	}
-}
-
-func (p *publisher) handleMessage(conn net.Conn) {
-	defer conn.Close()
-	var buf bytes.Buffer
-	io.Copy(&buf, conn)
-	if buf.Len() == 0 {
-		return
-	}
-	tagLength := 0
-	for {
-		currentByte := buf.Next(1)
-		tagLength += int(currentByte[0])
-		if currentByte[0] < 255 {
-			break
-		}
-	}
-	tag := string(buf.Next(tagLength))
-	payload := buf.Next(buf.Len())
-	message := NewMessage(tag)
-	message.Write(payload)
-	message.SetSender(conn.RemoteAddr().String())
-	p.Publish(message)
 }
--- publisher_test.go
+++ publisher_test.go
@@ -1,8 +1,6 @@
 package pub
 
 import (
-	"net"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -59,69 +57,6 @@ var _ = Describe("Publisher", func() {
 			input.Write(inputBytes)
 			pub.Publish(input)
 			Consistently(testSub.hasReceived).Should(BeFalse())
-		})
-	})
-
-	Context("gathering over tcp", func() {
-		longTag := `Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
-			Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec,
-			pellentesque eu, pretium quis,.`
-		var sub1 Subscriber
-		var sub2 Subscriber
-		remotePub := New()
-		remotePub.AddRemote("localhost:9000")
-		It("should publish the message to a single subscriber", func() {
-			sub1 = pub.Subscribe("taste", newTestSubscriber)
-			testSub1 := sub1.(*testSubscriber)
-			go pub.Gather(9000)
-			input = NewMessage("taste")
-			input.Write(inputBytes)
-			remotePub.Publish(input)
-			Eventually(testSub1.hasReceived).Should(BeTrue())
-		})
-
-		It("should not publish the message to the subscriber", func() {
-			pub.Unsubscribe("taste", sub1)
-			testSub1 := sub1.(*testSubscriber)
-			testSub1.received = false
-			input = NewMessage("taste")
-			input.Write(inputBytes)
-			pub.Publish(input)
-			Eventually(testSub1.hasReceived).Should(BeFalse())
-		})
-
-		It("should publish a message with a longer tag", func() {
-
-			sub1 = pub.Subscribe(longTag, newTestSubscriber)
-			testSub1 := sub1.(*testSubscriber)
-			input = NewMessage(longTag)
-			input.Write(inputBytes)
-			remotePub.Publish(input)
-			Eventually(testSub1.hasReceived).Should(BeTrue())
-		})
-
-		It("should publish a message to two subscribers", func() {
-			pub.Unsubscribe(longTag, sub1)
-			input = NewMessage("tuste")
-			input.Write(inputBytes)
-			sub1 = pub.Subscribe("tuste", newTestSubscriber)
-			sub2 = remotePub.Subscribe("tuste", newTestSubscriber)
-
-			testSub1 := sub1.(*testSubscriber)
-			testSub2 := sub2.(*testSubscriber)
-			remotePub.Publish(input)
-
-			Eventually(testSub1.hasReceived).Should(BeTrue())
-			Eventually(testSub2.hasReceived).Should(BeTrue())
-		})
-
-		It("should cancel gathering", func() {
-			pub.CancelGathering(9000)
-			conn, err := net.Dial("tcp", "localhost:9000")
-			if conn != nil {
-				defer conn.Close()
-			}
-			Expect(err).Should(HaveOccurred())
 		})
 	})
 })
